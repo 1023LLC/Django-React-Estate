0b4ba49595ef7f76af809e83f49f974c9d42ca82
julius <nyerereoffice@gmail.com>
2024-04-12 18:10:22 +0300

    Email Setup

--- backend/realest_estate/settings.py
+++ backend/realest_estate/settings.py
@@ -82,6 +82,16 @@ DATABASES = {
 }
 
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hiddenbookings58@gmail.com'
+EMAIL_HOST_PASSWORD = 'jivsoxfnfkvwoney'
+EMAIL_USE_TLS = True
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
